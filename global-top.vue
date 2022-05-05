a69fd42660065dc29c53b4f21e5df0f23c88f3e8
李伟民 <liweimin@cvte.com>
2022-05-06 01:58:27 +0800

    docs: 完事第一版

---
+++ global-top.vue
@@ -0,0 +1,15 @@
+<!-- global-top.vue -->
+<template>
+  <footer
+     v-if="$slidev.nav.currentLayout !== 'cover'"
+  >
+    <p class="absolute bottom-0 left-0 p-2">
+      💪 MinLee
+    </p>
+    <p
+     class="absolute bottom-0 right-0 p-2"
+    >
+       {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
+    </p>
+  </footer>
+</template>
